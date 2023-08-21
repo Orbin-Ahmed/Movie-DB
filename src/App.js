c80ade30890f272820b7196be22e2b328220cc86
Orbin23 <orbin.ahmed.acanto@g.bracu.ac.bd>
2023-08-21 15:23:53 +0600

    pushed

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ import MovieCard from './MovieCard';
 
 const API_URL = 'http://www.omdbapi.com/?apikey=9b97c2e7';
 
-const movie1 =
-{
-    "Title": "Spiderman and Grandma",
-    "Year": "2009",
-    "imdbID": "tt1433184",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg"
-}
-
 const App = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
